a9adab27dc48a68ec8b0c90589985c0bd1628cce
Manuela  Sanchez <m.sanchez947@hotmail.com>
2019-01-18 15:03:20 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -68,8 +68,7 @@ def adding_matz
 #    :languages => ["LISP", "C"]
 # }
 
-	programmer_hash =
-
+	programmer_hash = {
         :grace_hopper => {
           :known_for => "COBOL",
           :languages => ["COBOL", "FORTRAN"]
@@ -84,7 +83,7 @@ def adding_matz
         }
      }
 
-  
+
 end
 
 def changing_alan
